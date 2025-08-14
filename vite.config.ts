208d0eb301b63516d16f8110daae1f68a5c30c20
TestingAccountBee <bumbleprimebee013@gmail.com>
2025-08-14 10:32:57 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   base: "/pal-i-quiz-prep/",
-  plugins: [react()],
+  
+  server: {
+    host: "::",
+    port: 8080,
+  },
+
+  plugins: [
+    react(),
+    // Este plugin só será ativado em modo de desenvolvimento
+    mode === "development" && componentTagger(),
+  ].filter(Boolean), // Remove entradas falsas (como plugins inativos) do array
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
